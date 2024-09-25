4f5c76f44dc22442d3988ba30980baaff0f20fe7
kobel <kobelei335@gmail.com>
2024-09-24 21:44:36 -0700

    done

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # Exercise 1: Get connected and have a look at the food table
-#
 # Import sqlite3
 # Import the display function from visualizer.py
 # Connect to the food.db database
@@ -9,54 +8,88 @@
 # In the terminal, run this Python file
 # Open tables.html with Live Server
 
+import sqlite3
+from visualizer import display
 
+# Connect to the database
+conn = sqlite3.connect("food.db")
+cursor = conn.cursor()
 
-# Exercise 2 
+# Execute SELECT * query to retrieve all records
+cursor.execute("SELECT * FROM food")
+display(cursor)
+
+# Exercise 2
 # Write a SELECT query limiting output to 5 rows
 # Send the query to the db using execute
 # Call the display() function and pass it the output of execute
 # In the terminal, run this Python file
 # Open tables.html with Live Server
-# Edit the SELECT query so that the 5 rows retieved start at row 500
+# Edit the SELECT query so that the 5 rows retrieved start at row 500
 # In the terminal, run this Python file
 # Check output on tables.html
 
+# Query to retrieve 5 rows
+cursor.execute("SELECT * FROM food LIMIT 5")
+display(cursor)
 
+# Query to retrieve 5 rows starting at row 500
+cursor.execute("SELECT * FROM food LIMIT 5 OFFSET 500")
+display(cursor)
 
 # Exercise 3
 # Part 1
 # Write a SELECT query using ORDER BY with the Name column,
 # limit the results to 10
-# Execute the query, call the display() function, run the file, 
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
 # Part 2
 # Edit the SELECT query you just made using ORDER BY with the rowid column, specify DESC and limit the results to 10
 # Check the output
 # *In SQLite3, the index column, or Primary Key, is called rowid
 
+# Part 1: Query ordered by Name limited to 10
+cursor.execute("SELECT * FROM food ORDER BY Name LIMIT 10")
+display(cursor)
 
+# Part 2: Query ordered by rowid in descending order limited to 10
+cursor.execute("SELECT * FROM food ORDER BY rowid DESC LIMIT 10")
+display(cursor)
 
 # Exercise 4
 # Part 1
 # Create a SELECT query to retrieve the food names
-# * Make sure to spell the column heading exactly as you've seen it in previous results 
-# Execute the query, call the display() function, run the file, 
+# * Make sure to spell the column heading exactly as you've seen it in previous results
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
 # Part 2
-# Edit the SELECT query to retrieve all the food names and and species, limit results to 20
+# Edit the SELECT query to retrieve all the food names and species, limit results to 20
 # Check the output
 
+# Part 1: Query to retrieve food names
+cursor.execute("SELECT Name FROM food")
+display(cursor)
 
+# Part 2: Query to retrieve food names and species, limited to 20
+cursor.execute("SELECT Name, Species FROM food LIMIT 20")
+display(cursor)
 
 # Exercise 5: Get the name and species of all of the nuts
 # Create a SELECT query to retrieve the food names and species of nuts
-# Use a WHERE clause to specifiy nuts from the Group column
-# Execute the query, call the display() function, run the file, 
+# Use a WHERE clause to specify nuts from the Group column
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
 
+# Query to retrieve names and species of nuts
+cursor.execute("SELECT Name, Species FROM food WHERE \"Group\" = \"Nuts\"")
+display(cursor)
 
 # Exercise 6: Get all of the onions and herbs
-# Create a new SELECT query to retrieve the all of the herbs and onions
+# Create a new SELECT query to retrieve all of the herbs and onions
 # Use the Subgroup column to filter
-# Execute the query, call the display() function, run the file, 
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
+
+# Query to retrieve all herbs and onions
+cursor.execute("SELECT * FROM food WHERE Subgroup = \"Herbs\" OR Subgroup = \"Onions\"")
+display(cursor)
--- practice_session/practice.py
+++ practice_session/practice.py
@@ -4,42 +4,64 @@
 # Import display from visualizer.py
 # Connect to the simplefolks database and get a cursor
 
+import sqlite3
+from visualizer import display
 
+# Connect to the simplefolks.sqlite database
+conn = sqlite3.connect("simplefolks.sqlite")
+cursor = conn.cursor()
 
 # Exercise 2
-# Create a SELECT query to the pets table to retrieve 
+# Create a SELECT query to the pets table to retrieve
 # 5 pets beginning at rowid 11
 # Execute the query, call the display() function, run the file, and open tables.html with Live Server
 
-
+# Query to retrieve 5 pets starting from rowid 11
+cursor.execute("SELECT * FROM pets LIMIT 5 OFFSET 10")
+display(cursor)
 
 # Exercise 3
-# Create a SELECT query to the people table to retrieve 
+# Create a SELECT query to the people table to retrieve
 # just the name and age columns
 # Call the display function to show results on tables.html
 # Edit the SELECT query to show youngest to oldest
-# Execute the query, call the display() function, run the file, 
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
 
-
+# Query to retrieve name and age from the people table
+cursor.execute("SELECT name, age FROM people")
+display(cursor)
+# Query to retrieve name and age, ordered from youngest to oldest
+cursor.execute("SELECT name, age FROM people ORDER BY age ASC")
+display(cursor)
 
 # Exercise 4
-# Create a SELECT query to the people table to retrieve the 
+# Create a SELECT query to the people table to retrieve the
 # rowid and name columns, and
 # add a clause to put the names in reverse alphabetical order
-# Execute the query, call the display() function, run the file, 
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
 
-
+# Query to retrieve rowid and name, ordered in reverse alphabetical order
+cursor.execute("SELECT rowid, name FROM people ORDER BY name DESC")
+display(cursor)
 
 # Exercise 5
-# Create a SELECT query to retrieve all the cats in the pets table 
-# Execute the query, call the display() function, run the file, 
+# Create a SELECT query to retrieve all the cats in the pets table
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
 
+# Query to retrieve all cats from the pets table
+cursor.execute('SELECT * FROM pets WHERE type = "Cat"')
+display(cursor)
 
 # Exercise 6
-# Create a SELECT query to retrieve the first and last name of 
+# Create a SELECT query to retrieve the first and last name of
 # every senator in the politicians table
-# Execute the query, call the display() function, run the file, 
+# Execute the query, call the display() function, run the file,
 # and check output on tables.html
+
+# Query to retrieve first and last names of all senators from the politicians table
+cursor.execute("SELECT first_name, last_name FROM politicians WHERE position = 'Senator'")
+display(cursor)
+
